8b66676ca19864e11ad6e0b365bea1893cc669d7
Maxwell L <mleuthner2002@gmail.com>
2023-07-10 19:03:40 -0500

    added required packages

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
     'What would you like the title to be?',
     'What is the description of you application?',
     'How would a user install your application?',
+    'What are the contribution guidelines?',
     'What are the Instructions for testing?',
     'What is your GitHub username?',
     'What is your email address?'
@@ -14,7 +17,9 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
