803137c47830856db3df1898796bf6eabfaddb7b
Maxwell L <mleuthner2002@gmail.com>
2023-07-10 19:37:58 -0500

    markdown file si now created from the command line

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -14,11 +15,18 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, '', function(err){
+        if(err) throw err;
+    });
+    fs.appendFile(`./${fileName}`, 'added2', (err) =>
+    err ? console.error(err) : console.log('Commit logged!'));
+}
 
 // TODO: Create a function to initialize app
 function init() {
-
+    var data;
+    writeToFile('README.md', data);
 }
 
 // Function call to initialize app
